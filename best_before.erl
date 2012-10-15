b1bd125525b9e9f45b513b03e1ef2125cee94c84
rafanoronha <rafanoronha@gmail.com>
2012-10-15 19:00:42 -0300

    implementing some stuff.not sure exactly what. missing TDD in 3,2,1...

--- best_before.erl
+++ best_before.erl
@@ -1,10 +1,42 @@
 -module(best_before).
--export([parse_string/1]).
+-compile(export_all).
 
 parse_string(S) ->
   parse_bin(list_to_binary(S)).
 
-parse_bin(<<A:2/binary, "/", B:2/binary, "/", C:4/binary>>) ->
-  ok;
+parse_bin(<<A:2/binary, "/", B:2/binary, "/", Year:4/binary>>) ->
+  [YearNum, ANum, BNum] = lists:map(fun bin_to_int/1, [Year,A,B]),
+  Date = parse_date(YearNum, [ANum, BNum]),
+  case Date of
+    { error, _ } ->
+      nok;
+    _ValidDate   ->
+      format_date(Date)
+  end;
 parse_bin(_) ->
   nok.
+
+parse_date(Year, [A|[B]]) ->
+  case eligible_to_month([A|[B]]) of
+    [] ->
+      { error, invalid_date };
+    [_H|_T] -> 
+      [Month, Day] = lists:sort([A, B]),
+      { Year, Month, Day }
+  end.
+parse_date([A|[B|[C]]]) ->
+  { error, invalid_date }.
+
+format_date({ Year, Month, Day }) ->
+  integer_to_list(Year) ++ "/" ++ integer_to_list(Month) ++ "/" ++ integer_to_list(Day).
+
+is_month(Number) ->
+  (12 >= Number) and (1 =< Number).
+
+eligible_to_month(L) when is_list(L) ->
+  lists:filter(fun is_month/1, L).
+
+bin_to_int(Bin) ->
+  { Int, [] } = string:to_integer(binary_to_list(Bin)),
+  Int.
+  
